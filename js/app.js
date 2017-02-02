7d5c1c6eefc502646f29192e75f6904541ca421b
puan0601 <anton.pugach@gmail.com>
2017-02-01 20:51:43 -0800

    added buttons and fixed layout, working on move controller

--- js/app.js
+++ js/app.js
@@ -4,7 +4,20 @@ $(document).ready(function() {
   $('.p1').click(function clickHandler() {
     $(this).css('background', 'blue');
   });
+
   $('.p2').click(function clickHandler() {
     $(this).css('background', 'red');
   });
+
+  $('#reset').click(function resetHandler() {
+    $('.box').html("");
+  });
+
+  //create players objects
+
+  //start game
+
+  //movement controller
+
+  //win logic
 });
