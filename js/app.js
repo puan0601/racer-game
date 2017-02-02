a7938a1ef1e7e42585c944549b2a8efcabf85539
puan0601 <anton.pugach@gmail.com>
2017-02-01 23:43:10 -0800

    finished key generator, worked out display logic for keys

--- js/app.js
+++ js/app.js
@@ -1,3 +1,60 @@
+//create players object constructor
+function createPlayer(name) {
+  this.name = name;
+  this.counter = 0;
+}
+
+//start game
+function startGame() {
+  var player1 = new createPlayer("P1");
+  var player2 = new createPlayer("P2");
+  var moves = generateRandomMoves();
+  setTimeout(countDown, 1000);
+  populateButton(moves);
+  // populateButton();
+
+
+}
+
+function generateRandomMoves() {
+  var moves = [];
+  while (moves.length < 12) {
+    var rando = Math.floor(Math.random() * (222 - 48 + 1) + 48); //(max - min + 1) + min    222, 48 is max,min for keyboard vals
+    var stringRando = rando.toString();
+    if (Object.keys(keyCodes).includes((stringRando))) {
+      moves.push(keyCodes[rando]);
+    }
+  }
+  return moves;
+}
+
+
+//not quite working, only prints final "GO!", not actual countdown
+function countDown(count){
+   if(count > 0){
+     var str = count.toString();
+     $('#countdown').text(str);
+     setTimeout(function() {countDown(count-1); },1000);
+   } else {
+     $('#countdown').text("GO!");
+   }
+}
+
+function populateButton(moves) {
+  var nextMove = moves.shift();
+  var idx = 0
+  $('#'+idx).text(nextMove);
+  idx++
+  if (idx === 12) {
+    return
+  }
+}
+
+
+
+//movement controller
+
+//win logic
 console.log('js loaded');
 // wait for the DOM to finish loading
 $(document).ready(function() {
@@ -16,26 +73,13 @@ $(document).ready(function() {
   $(window).keydown(function handleEvent(event) {
     var key = event.keyCode;
     console.log(key);
-
   });
 
-  //create players object constructor
-  function Player(name) {
-    this.name = name;
-    this.counter = 0;
-  }
+  $('#start').click(function buttonHandler() {
+    startGame();
+  });
 
-  //start game
-  function startGame() {
-    var player1 = new Player("P1");
-    var player2 = new Player("P2");
-    console.log(player1);
-    console.log(player2);
-  }
-  startGame();
-
-
-  //movement controller
-
-  //win logic
+  $('.p2').click(function clickHandler() {
+    $(this).css('background', 'red');
+  });
 });
