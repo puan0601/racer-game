2877a57238abda18255ce0daa72e6b39d32a7619
puan0601 <anton.pugach@gmail.com>
2017-02-01 16:23:16 -0800

    added click controller for board

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 console.log('js loaded');
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  
+  $('.box').click(function clickHandler() {
+    $(this).css('background', 'blue');
+});
 });
