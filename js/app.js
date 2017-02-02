fcdb84be23fe8736e4b7d9f2750246d4db261466
puan0601 <anton.pugach@gmail.com>
2017-02-02 01:50:49 -0800

    fixed racing logic

--- js/app.js
+++ js/app.js
@@ -1,56 +1,47 @@
 //create players object constructor
-function createPlayer(name) {
+function Player(name) {
   this.name = name;
-  this.counter = 0;
+  this.position = 0;
+  this.key = [];
 }
-
-//start game
-function startGame() {
-  var player1 = new createPlayer("P1");
-  var player2 = new createPlayer("P2");
-  var moves = generateRandomMoves();
-  setTimeout(countDown, 1000);
-  populateButton(moves);
-  // populateButton();
-
-
-}
-
-function generateRandomMoves() {
-  var moves = [];
-  while (moves.length < 12) {
+function generateRandomKey(player) {
+  var tempkey = [];
+  while (tempkey.length === 0) {
     var rando = Math.floor(Math.random() * (222 - 48 + 1) + 48); //(max - min + 1) + min    222, 48 is max,min for keyboard vals
     var stringRando = rando.toString();
     if (Object.keys(keyCodes).includes((stringRando))) {
-      moves.push(keyCodes[rando]);
+      player.key.push(rando);
+      tempkey.push(keyCodes[rando]);
     }
   }
-  return moves;
+  return tempkey;
 }
 
-
-//not quite working, only prints final "GO!", not actual countdown
 function countDown(count){
    if(count > 0){
      var str = count.toString();
      $('#countdown').text(str);
      setTimeout(function() {countDown(count-1); },1000);
    } else {
-     $('#countdown').text("GO!");
+     populateButton(player1);
+     populateButton(player2);
    }
 }
 
-function populateButton(moves) {
-  var nextMove = moves.shift();
-  var idx = 0
-  $('#'+idx).text(nextMove);
-  idx++
-  if (idx === 12) {
-    return
+function populateButton(player) {
+  if (player === player1) {
+    var position = player1.position;
+    var move = generateRandomKey(player);
+    $('#a'+position).text(move);
+    player1.position++
+  } else {
+    var position = player2.position;
+    var move = generateRandomKey(player);
+    $('#b'+position).text(move);
+    player2.position++
   }
+
 }
-
-
 
 //movement controller
 
@@ -58,14 +49,6 @@ function populateButton(moves) {
 console.log('js loaded');
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  $('.p1').click(function clickHandler() {
-    $(this).css('background', 'blue');
-  });
-
-  $('.p2').click(function clickHandler() {
-    $(this).css('background', 'red');
-  });
-
   $('#reset').click(function resetHandler() {
     $('.box').html("");
   });
@@ -73,13 +56,28 @@ $(document).ready(function() {
   $(window).keydown(function handleEvent(event) {
     var key = event.keyCode;
     console.log(key);
+    var pos1 = player1.position - 1;
+    var pos2 = player2.position - 1;
+    console.log(pos1, pos2);
+    if (key === player1.key[pos1]) {
+      $('#a'+pos1).css('background', 'blue');
+      populateButton(player1);
+    } else if (key === player2.key[pos2]) {
+      $('#b'+pos2).css('background', 'red');
+      populateButton(player2);
+    }
   });
 
   $('#start').click(function buttonHandler() {
-    startGame();
-  });
+    //intentionally removed "var" to put players into global scope.
+    player1 = new Player("P1");
+    player2 = new Player("P2");
+    countDown(3);
+    if (player1.position === 6) {
+      $('#countdown').text("Player 1 WINS!!!!!!!!");
+    } else if (player2.position === 6) {
+      $('#countdown').text("Player 2 WINS!!!!!!!!");
+    }
 
-  $('.p2').click(function clickHandler() {
-    $(this).css('background', 'red');
   });
 });
