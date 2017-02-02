34ae7b6333bfc7a4c66d3b817b5dca34789d095f
puan0601 <anton.pugach@gmail.com>
2017-02-01 16:50:23 -0800

    added 2nd player

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 console.log('js loaded');
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  $('.box').click(function clickHandler() {
+  $('.p1').click(function clickHandler() {
     $(this).css('background', 'blue');
-});
+  });
+  $('.p2').click(function clickHandler() {
+    $(this).css('background', 'red');
+  });
 });
